fce470b4a0b9201c506eac0f1371ea81d42802b3
CaioVIN30 <caioviniciuslemos22@gmail.com>
2025-05-01 01:07:22 -0300

    correção exercício 04

--- ex004.py
+++ ex004.py
@@ -1,9 +1,10 @@
 #Faça um programa que leia algo pelo teclado e mostre o seu tipo primitivo e todas as informações possíveis sobre ele.
 
-n = int(input('Digite algo: '))
-print(type(n))
-print(n.isnumeric())
-print(n.isalpha())
-print(n.isalnum())
-print(n.isupper())
-print(n.islower())
+n = (input('Digite algo: '))
+print('O tipo primitivo dessa variável é:', type(n))
+print('É um número? ', n.isnumeric())
+print('É uma letra? ', n.isalpha())
+print('É alfanúmerico? ', n.isalnum())
+print('Está em maiúsculo? ', n.isupper())
+print('Está em minúsculo? ', n.islower())
+print('Está capitalizada?', n.istitle())
